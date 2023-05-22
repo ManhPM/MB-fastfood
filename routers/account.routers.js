10b0fd37adc4366855a498d298e7d491bcf65ab4
ManhPM <phammanhbeo2001@gmail.com>
2023-05-22 17:35:21 +0700

    GK-Mobile

--- routers/account.routers.js
+++ routers/account.routers.js
@@ -1,23 +1,11 @@
 const express = require("express");
-const {Account, Customer} = require("../models")
-const {login, uploadAvatar, logout, createAccountForCustomer, changePassword, forgotPassword, loginAdmin, verify, accessForgotPassword, updateProfile} = require("../controllers/account.controllers");
+const {Account} = require("../models")
+const {login} = require("../controllers/account.controllers");
 const { checkExistAccount } = require("../middlewares/validates/checkExist");
-const { checkCreateAccount, checkPhone, checkEmail } = require("../middlewares/validates/checkCreate");
-const {authenticate} = require("../middlewares/auth/authenticate.js")
-const {authorize} = require("../middlewares/auth/authorize.js")
 
 const accountRouter = express.Router();
 
 accountRouter.post("/login", checkExistAccount(Account), login);
-accountRouter.post("/admin/login", checkExistAccount(Account), loginAdmin);
-accountRouter.get("/logout", authenticate, logout);
-accountRouter.post("/avatar", authenticate, authorize(["Khách hàng"]), uploadAvatar);
-accountRouter.put("/updateprofile", authenticate, authorize(["Khách hàng"]), updateProfile);
-accountRouter.post("/create", checkCreateAccount(Account), checkEmail(Customer), checkPhone(Customer), createAccountForCustomer);
-accountRouter.post("/forgotpassword", checkExistAccount(Account), forgotPassword);
-accountRouter.post("/forgotpassword/verify", checkExistAccount(Account), verify);
-accountRouter.post("/forgotpassword/verify/success", checkExistAccount(Account), accessForgotPassword);
-accountRouter.put("/changepassword", authenticate, changePassword);
 
 module.exports = {
     accountRouter,
--- routers/order.routers.js
+++ routers/order.routers.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const {authenticate} = require("../middlewares/auth/authenticate.js")
 const {authorize} = require("../middlewares/auth/authorize.js");
-const { getAllItemInOrder, getAllOrder, confirmOrder, cancelOrder, thongKeSanPham, thongKeDonHang, chart } = require("../controllers/order.controllers");
+const { getAllItemInOrder, getAllOrder } = require("../controllers/order.controllers");
 const orderRouter = express.Router();
 
 orderRouter.get("/", authenticate, authorize(["Khách hàng","Admin"]), getAllOrder);
 orderRouter.get("/detail/:id_order", authenticate, authorize(["Khách hàng","Admin"]), getAllItemInOrder);
-orderRouter.get("/confirm/:id_order", authenticate, authorize(["Admin"]), confirmOrder);
-orderRouter.get("/cancel/:id_order", authenticate, authorize(["Admin","Khách hàng"]), cancelOrder);
-orderRouter.get("/thongke", authenticate, authorize(["Admin"]), thongKeSanPham);
-orderRouter.get("/thongkedonhang", authenticate, authorize(["Admin"]), thongKeDonHang);
-orderRouter.get("/chart", authenticate, authorize(["Khách hàng"]), chart);
 
 
 module.exports = {
